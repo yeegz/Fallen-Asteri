95a11a0d161fa03b5bd27af9d0682cfbeadc2671
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-04-24 09:47:37 +0800

    Added movement, fix map issue where sprite fall off the edge of the map

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -1,15 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 30
-const JUMP = 30
-var GRAVITY_VALUE = 980
+const SPEED = 300
+const JUMP = -650
+var GRAVITY_VALUE = 1100
 
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#Gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY_VALUE * delta
-		move_and_slide()
+	
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP
+	
+	#Added buttons A and D to the left and right axis respectively
+	var direction = Input.get_axis("ui_left", "ui_right")
+	#Movement. Note: character doesn't stop moving in one direction until opposite button
+	#is pressed.
+	if direction:
+		velocity.x = SPEED * direction
+	
+	
+	
+	
+	move_and_slide()
 
