69d6e13de6a2e59b5b1ca1b234e6e6bddab79c90
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-04-30 08:56:50 +0800

    add attack to ui actions in project settings

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 300
 const JUMP = -550
 var GRAVITY_VALUE = 1100
+var PLAYER_HP = 100
+var PLAYER_STAMINA = 100
 @onready var animation = $AnimationPlayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,3 +40,9 @@ func _process(delta):
 		animation.play("idle")
 	else:
 		animation.stop()
+	
+	#Attack
+	if Input.is_action_just_pressed("ui_attack") and direction == 1:
+		print("Attack to right")
+	elif Input.is_action_just_pressed("ui_attack") and direction == -1:
+		print("Attack to left")
