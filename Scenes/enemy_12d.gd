5098938dad02b68801e97ac3900f7d3961af4047
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-01 23:41:12 +0800

    rework done; added basic enemy pathing

--- Scenes/enemy_12d.gd
+++ Scenes/enemy_12d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const GRAVITY_VALUE = 1100
-var SPEED = 250
+var SPEED = 50
 var player = null
 var player_chase = false
 
@@ -10,15 +10,17 @@ func _process(delta):
 	#Gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY_VALUE * delta
-	
 	move_and_slide()
 
 
 func _on_detection_area_body_entered(body):
-	pass # Replace with function body.
-	
-
-
+	player = body
+	player_chase = true
 
 func _on_detection_area_body_exited(body):
-	pass # Replace with function body.
+	player = null
+	player_chase = false
+
+func _physics_process(delta):
+	if player_chase == true:
+		position += (player.position - position)/ SPEED
