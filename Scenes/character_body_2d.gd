4c5a672d899f5762de69df64a28288a91b304b7c
Yousof Selim <yousofselim2@gmail.com>
2024-05-01 18:31:03 +0800

    create and implement jump animation and implement scaling

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -17,6 +17,8 @@ func _process(delta):
 	#Jump
 	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP
+	if velocity.y != 0:
+		animation.play("jump")
 	
 	#Added buttons A and D to the left and right axis respectively
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -24,24 +26,26 @@ func _process(delta):
 	#Movement. 
 	if direction:
 		velocity.x = SPEED * direction
-		animation.play("run")
+		
 		if direction == 1:
 			$AnimatedSprite2D.flip_h = false
 		elif direction == -1:
 			$AnimatedSprite2D.flip_h = true
 	elif direction == 0 or velocity.x == 0:
-		animation.play("idle")
-		
-		
 		
 		#requires move_towards to enable stopping movement
 		velocity.x  = move_toward(1, 0, 1)
+	
 	
 	#move_and_slide required for basic physics functions to work
 	move_and_slide()
 	
 	#Animation
-	if velocity.x == 0:
+	if direction and velocity .y == 0:
+		animation.play("run")
+	elif velocity.y != 0:
+		animation.play("jump")
+	elif direction == 0 or velocity.x ==0:
 		animation.play("idle")
 	
 	
@@ -50,3 +54,4 @@ func _process(delta):
 		print("Attack to right")
 	elif Input.is_action_just_pressed("ui_attack") and direction == -1:
 		print("Attack to left")
+	
