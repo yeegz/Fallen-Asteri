3e26c88852b5dd15e377387cb170fee9b17e26e7
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-01 23:18:17 +0800

    Added everything to wrong script. Reworking now

--- Scenes/enemy_12d.gd
+++ Scenes/enemy_12d.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 const GRAVITY_VALUE = 1100
-
+var SPEED = 250
+var player = null
+var player_chase = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -10,3 +12,13 @@ func _process(delta):
 		velocity.y += GRAVITY_VALUE * delta
 	
 	move_and_slide()
+
+
+func _on_detection_area_body_entered(body):
+	pass # Replace with function body.
+	
+
+
+
+func _on_detection_area_body_exited(body):
+	pass # Replace with function body.
