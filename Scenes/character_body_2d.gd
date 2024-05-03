0f5b871f2eaadd6fabef80e5039b573cd56883f7
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-03 23:24:42 +0800

    Clean up code, turn into functions

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -1,26 +1,38 @@
 extends CharacterBody2D
 
+#variable declaration
 const SPEED = 300
 const JUMP = -550
 var GRAVITY_VALUE = 1100
 var PLAYER_HP = 100
 var PLAYER_STAMINA = 100
+var player_alive = true
 @onready var animation = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#Gravity
-	if not is_on_floor():
-		velocity.y += GRAVITY_VALUE * delta
+	var control = controls(delta)
+	var grav = gravity(delta)
+	var anim = animations(control)
+	
+	#Attack
+	if Input.is_action_just_pressed("ui_attack") and control == 1:
+		print("Attack to right")
+	elif Input.is_action_just_pressed("ui_attack") and control == -1:
+		print("Attack to left")
+
+#Easiest way for enemy hitbox to idintify player is through methods. Creating throwaway
+#function temporarily
+func hero():
+	pass
+
+func controls(delta):
+	#Added buttons A and D to the left and right axis respectively
+	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	#Jump
 	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP
-	if velocity.y != 0:
-		animation.play("jump")
-	
-	#Added buttons A and D to the left and right axis respectively
-	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	#Movement. 
 	if direction:
@@ -35,23 +47,20 @@ func _process(delta):
 		#requires move_towards to enable stopping movement
 		velocity.x  = move_toward(1, 0, delta)
 	
-	
 	#move_and_slide required for basic physics functions to work
 	move_and_slide()
-	
+	return direction
+
+func gravity(delta):
+	#Gravity
+	if not is_on_floor():
+		velocity.y += GRAVITY_VALUE * delta
+
+func animations(control):
 	#Animation
-	if direction and velocity .y == 0:
+	if control and velocity.y == 0:
 		animation.play("run")
 	elif velocity.y != 0:
 		animation.play("jump")
-	elif direction == 0 or velocity.x ==0:
+	elif control == 0 or velocity.x == 0:
 		animation.play("idle")
-	
-	
-	#Attack
-	if Input.is_action_just_pressed("ui_attack") and direction == 1:
-		print("Attack to right")
-	elif Input.is_action_just_pressed("ui_attack") and direction == -1:
-		print("Attack to left")
-	
-
--- Scenes/enemy_12d.gd
+++ Scenes/enemy_12d.gd
@@ -3,42 +3,63 @@ extends CharacterBody2D
 const GRAVITY_VALUE = 1100
 var SPEED = 70
 var JUMP = -500
-var ENEMY_HP = 90
+var ENEMY_HP = 100
 var player = null
 var player_chase = false
+var attack_range = false
+var attack_cooldown = true
 @onready var animation = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+
+func _physics_process(delta):
+	var grav = gravity(delta)
+	enemy_attack()
+	var anim = animations(player_chase)
+	var path = pathing(player_chase, delta, SPEED, JUMP)
+
+#when player enters DetectionArea
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+#when player exits detection area
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("hero"):
+		attack_range = true
+
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("hero"):
+		attack_range = false
+
+func enemy_attack():
+	if attack_range == true:
+		print("Player taking damage")
+
+func gravity(delta):
 	#Gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY_VALUE * delta
 	move_and_slide()
 
-
-func _on_detection_area_body_entered(body):
-	player = body
-	player_chase = true
-
-func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
-
-func _physics_process(delta):
-	if player_chase == true:
-		position += (player.position - position)/ SPEED
-		move_toward(1,0,delta)
-		if not player.is_on_floor() and is_on_floor():
-			velocity.y = JUMP
-			
-		
-	
+func animations(player_chase):
 	#Animation
 	if player_chase == false:
 		animation.play("crab_idle")
 	else:
 		animation.play("crab_attack")
-	
-	
 
-
+func pathing(player_chase, delta, SPEED, JUMP):
+	if player_chase == true:
+		#move towards player if in detection area
+		position += (player.position - position)/ SPEED
+		#to prevent enemy from sliding
+		move_toward(1, 0, delta)
+		#jumping func
+		if not player.is_on_floor() and is_on_floor():
+			velocity.y = JUMP
