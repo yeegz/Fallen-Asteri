e29e7e1210f0c0c06894c93478d4347b9b74ff11
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-08 21:57:38 +0800

    fix a bug where stamina regenerates above maximum limit

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -9,6 +9,7 @@ var PLAYER_HP = 100
 var PLAYER_STAMINA = 100
 var player_attack_range = false
 var player_attack_cooldown = true
+var stamina_requirement = 30
 @onready var animation = $AnimatedSprite2D
 
 #main function
@@ -17,6 +18,7 @@ func _process(delta):
 	var grav = gravity(delta)
 	var anim = animations(control)
 	player_attack()
+	print(PLAYER_STAMINA)
 
 #Easiest way for enemy hitbox to idintify player is through methods. Creating throwaway
 #function temporarily
@@ -78,7 +80,6 @@ func _on_player_attack_range_body_exited(body):
 		player_attack_range = false
 
 func player_attack():
-	var stamina_requirement = 30
 	if Input.is_action_just_pressed("ui_attack") and player_attack_range == true and player_attack_cooldown == true and PLAYER_STAMINA > stamina_requirement:
 		enemy.ENEMY_HP -= 20
 		PLAYER_STAMINA -= stamina_requirement
@@ -93,4 +94,5 @@ func _on_player_cooldown_timeout():
 
 
 func _on_player_stamina_timeout():
-	PLAYER_STAMINA += 10
+	if PLAYER_STAMINA < 100:
+		PLAYER_STAMINA += 10
