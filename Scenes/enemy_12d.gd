2be396eea153f01f678fb49eeff17b31dbe2a9e0
Yousof Selim <yousofselim2@gmail.com>
2024-05-15 13:33:52 +0800

    add life steal system after killing enemy

--- Scenes/enemy_12d.gd
+++ Scenes/enemy_12d.gd
@@ -102,6 +102,7 @@ func _on_cooldown_timeout():
 #handles enemy death. Essentially deletes the sprite off the scene if hp = 0 or less
 func death():
 	if ENEMY_HP <= 0:
+		player.PLAYER_HP = 200
 		queue_free()
 
 #links enemy healthbar GUI to ENEMY_HP
