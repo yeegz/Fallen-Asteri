b35e9fa95742c9fdd87c461eec5ce8d6dd4eb1d0
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-18 22:45:53 +0800

    add scene transition from scene 2 to scene 1

--- scene_transition_scene_2-1.gd
+++ scene_transition_scene_2-1.gd
@@ -15,4 +15,4 @@ func _on_body_exited(body):
 
 func _physics_process(delta):
 	if player != null:
-		get_tree().change_scene_to_file("res://forest.tscn")
+		get_tree().change_scene_to_file("res://scene.tscn")
