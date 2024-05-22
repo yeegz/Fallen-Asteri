25dc092fb0b2b9d3881e0fa310e7c9abc9d60c22
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-22 23:38:21 +0800

    add basic pathing to enemy

--- Scenes/enemy_scene_2.gd
+++ Scenes/enemy_scene_2.gd
@@ -4,9 +4,18 @@ extends CharacterBody2D
 const GRAVITY_VALUE = 1100
 var SPEED = 70
 var JUMP = -500
+var ENEMY_HP = 60
+var player = null
+var player_chase = false
+var attack_range = false
+var attack_cooldown = true
 
 func enemy():
 	pass
+
+func _physics_process(delta):
+	gravity(delta)
+	pathing(player_chase, delta, SPEED)
 
 func gravity(delta):
 	#Gravity
@@ -14,6 +23,31 @@ func gravity(delta):
 		velocity.y += GRAVITY_VALUE * delta
 	move_and_slide()
 
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
 
-func _physics_process(delta):
-	gravity(delta)
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+#pathfinding, knockback
+func pathing(playerchase, delta, speed):
+	if playerchase == true:
+		
+		#move towards player if in detection area
+		position += (player.position - position)/ speed
+		#to prevent enemy from sliding
+		move_toward(1, 0, delta)
+		
+		#Flip sprite according to direction
+		if player.position < position:
+			$AnimatedSprite2D.flip_h = true
+		elif player.position > position:
+			$AnimatedSprite2D.flip_h = false
+		
+		#Knockback
+		#if player.position < position and attack_range == true and attack_cooldown == true:
+			#player.position.x += -knockback * delta
+		#elif player.position > position and attack_range == true and attack_cooldown == true:
+			#player.position.x += knockback * delta
