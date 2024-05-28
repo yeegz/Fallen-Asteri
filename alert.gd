a9d1b8bc64b67b89e1411c5fa820eec049160947
Yoshenan <yosh45650@gmail.com>
2024-05-28 12:00:48 +0800

    added progress bar in character body 2d and fixed bug in alert screen

--- alert.gd
+++ alert.gd
@@ -4,8 +4,8 @@ extends Control
 
 
 func _on_yes_pressed():
-	get_tree().change_scene_to_file("res://main menu.tscn")
+	get_tree().change_scene_to_file("res://scene.tscn")
 
 
 func _on_no_pressed():
-	get_tree().change_scene_to_file("res://scene.tscn")
+	get_tree().change_scene_to_file("res://main menu.tscn")
