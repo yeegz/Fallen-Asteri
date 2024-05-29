9c99756541bffba719b9c430a6072a0747f655ed
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-05-29 12:12:26 +0800

    add basic pathing

--- Scenes/sideboss_2d.gd
+++ Scenes/sideboss_2d.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 #variable declaration
 const GRAVITY_VALUE = 1100
+var SPEED = 70
+var player_chase = false
+var attack_range = false
+var player = null
 
 func _physics_process(delta):
 	gravity(delta)
+	pathing(player_chase,delta,SPEED)
 
 func enemy():
 	pass
@@ -14,3 +19,33 @@ func gravity(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY_VALUE * delta
 	move_and_slide()
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+#pathfinding, knockback
+func pathing(playerchase, delta, speed):
+	if playerchase == true:
+		
+		#move towards player if in detection area
+		position += (player.position - position)/ speed
+		#to prevent enemy from sliding
+		move_toward(1, 0, delta)
+		
+		#Flip sprite according to direction
+		if player.position < position:
+			$AnimatedSprite2D2.flip_h = true
+		elif player.position > position:
+			$AnimatedSprite2D2.flip_h = false
+		
+		#Knockback
+		#if player.position < position and attack_range == true and attack_cooldown == true:
+			#player.position.x += -knockback * delta
+		#elif player.position > position and attack_range == true and attack_cooldown == true:
+			#player.position.x += knockback * delta
