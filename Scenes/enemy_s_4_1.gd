0d93906673fe758bbbe7ca6b43e252f1ad97650b
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-06-05 21:33:11 +0800

    add pathfinding to enemy

--- Scenes/enemy_s_4_1.gd
+++ Scenes/enemy_s_4_1.gd
@@ -26,3 +26,34 @@ func gravity(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY_VALUE * delta
 	move_and_slide()
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+#pathfinding, knockback
+func pathing(playerchase, delta, speed):
+	if playerchase == true:
+		
+		#move towards player if in detection area
+		position += (player.position - position)/ speed
+		#to prevent enemy from sliding
+		move_toward(1, 0, delta)
+		
+		#Flip sprite according to direction
+		if player.position < position:
+			$AnimatedSprite2D.flip_h = true
+		elif player.position > position:
+			$AnimatedSprite2D.flip_h = false
+		
+		#Knockback
+		#if player.position < position and attack_range == true and attack_cooldown == true:
+			#player.position.x += -knockback * delta
+		#elif player.position > position and attack_range == true and attack_cooldown == true:
+			#player.position.x += knockback * delta
