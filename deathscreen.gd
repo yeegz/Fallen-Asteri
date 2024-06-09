6608d8ad95802dcce967eb2b27ea8e51a502979d
Yoshenan <yosh45650@gmail.com>
2024-06-09 18:49:18 +0800

    modified resolution and window mode

--- deathscreen.gd
+++ deathscreen.gd
@@ -9,11 +9,14 @@ func _on_restart_pressed():
 
 
 
-func _on_quit_pressed():
-	get_tree().quit()
+
 
 func music():
 	death_sound.play()
 
 func _physics_process(delta):
 	music()
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://main menu.tscn")
