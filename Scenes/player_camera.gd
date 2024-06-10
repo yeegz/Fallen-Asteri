d354745a70945920091c3d38f13654483459b070
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-06-10 23:18:00 +0800

    add dynamic tracking for camera

--- Scenes/player_camera.gd
+++ Scenes/player_camera.gd
@@ -1,7 +1,18 @@
 extends Camera2D
 
+
+
 func _process(delta):
 	camera_tracking()
 
 func camera_tracking():
 	position = global.PLAYER_POSITION
+	#x position
+	if position.x <= 13:
+		position.x = 13
+	elif position.x >= 1097.9:
+		position.x = 1097.9
+	
+	#y position
+	if position.y <= 74:
+		position.y = 74
